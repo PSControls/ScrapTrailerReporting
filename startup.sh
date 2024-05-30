cda09fd2ecffed5c059ec51c7997939215d83803
PSControls <gray.oxley@martinrea.com>
2024-05-30 15:35:50 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 mkdir ~/ScrapTrailerReporting
 cd ~/ScrapTrailerReporting
 curl ~/ScrapTrailerReporting/Dockerfile https://raw.githubusercontent.com/PSControls/ScrapTrailerReporting/master/Dockerfile 
-sudo docker build --no-cache -t node-red-project .
-sudo docker run --name nr-container -p 1880:1880 node-red-project
+docker container prune
+docker build --no-cache -t node-red-project .
+docker run --name nr-container -p 1880:1880 node-red-project
