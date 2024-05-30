b6cde3df3d0f72a29519c4782ae016a6bfe983bb
PSControls <gray.oxley@martinrea.com>
2024-05-30 15:36:20 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 mkdir ~/ScrapTrailerReporting
 cd ~/ScrapTrailerReporting
-curl ~/ScrapTrailerReporting/Dockerfile https://raw.githubusercontent.com/PSControls/ScrapTrailerReporting/master/Dockerfile 
+curl -o ~/ScrapTrailerReporting/Dockerfile https://raw.githubusercontent.com/PSControls/ScrapTrailerReporting/master/Dockerfile 
 docker container prune
 docker build --no-cache -t node-red-project .
 docker run --name nr-container -p 1880:1880 node-red-project
