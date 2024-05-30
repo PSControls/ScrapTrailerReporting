f79fca232abdfce0e6d452f0d7acaa6b81bb9d1b
PSControls <gray.oxley@martinrea.com>
2024-05-30 16:07:13 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 mkdir $HOME/nr-projects/ScrapTrailerReporting
 cd $HOME/nr-projects/ScrapTrailerReporting
 curl -o $HOME/nr-projects/ScrapTrailerReporting/Dockerfile https://raw.githubusercontent.com/PSControls/ScrapTrailerReporting/master/Dockerfile 
-docker container prune -y
 docker build --no-cache -t ScrapTrailerReporting .
 docker run --name nr-container -p 1880:1880 ScrapTrailerReporting
